068e944ca3c7addcae73ceef6b2b33fe04934458
Affuyfuffyt <98175855+Affuyfuffyt@users.noreply.github.com>
2026-01-04 17:11:36 +0300

    Update installl.sh

--- installl.sh
+++ installl.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# تنظيف شامل للملفات والجلسات القديمة
-rm -rf /root/tg_pro_hunter
+# تنظيف شامل لكل ملفات الجلسات القديمة (لأن الحظر يلتصق بملف الجلسة)
+rm -rf /root/hunter_stealth
 rm -f /root/*.session
-mkdir /root/tg_pro_hunter
-cd /root/tg_pro_hunter
+mkdir /root/hunter_stealth
+cd /root/hunter_stealth
 
-echo "------------------------------------------------"
-echo "   جاري تثبيت الصائد الاحترافي ببيانات الـ API الجديدة   "
-echo "------------------------------------------------"
+echo "--- تثبيت الصائد الخفي (نسخة الحماية القصوى) ---"
 
-# تثبيت المتطلبات
 apt update && apt upgrade -y
 apt install -y python3 python3-pip curl
 pip3 install telethon
 
-# تحميل الكود من مستودعك
 curl -Ls https://raw.githubusercontent.com/Affuyfuffyt/Blustwin1/refs/heads/main/bot_core.py -o bot_core.py
 
-# طلب البيانات الأساسية
-read -p "🎯 أدخل Token البوت: " token
-read -p "🎯 أدخل ID حسابك (Admin): " adminid
+read -p "🎯 Token: " token
+read -p "🎯 Admin ID: " adminid
 
-# حقن البيانات
 sed -i "s/TOKEN_HERE/$token/g" bot_core.py
 sed -i "s/ADMIN_ID_HERE/$adminid/g" bot_core.py
 
-echo "------------------------------------------------"
-echo "  سيطلب منك الآن الرقم والكود لتسجيل الدخول   "
-echo "------------------------------------------------"
-
+echo "--- جارِ التشغيل.. أدخل الرقم والكود ---"
 python3 bot_core.py
